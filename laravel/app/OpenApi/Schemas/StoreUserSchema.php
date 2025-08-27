d04e201356c9819771e38161b42b8c917a6f748c
watanabe-yuya <yuya.watanabe@t8s.co.jp>
2025-08-27 11:08:34 +0900

    ユーザーデータ作成スキーマ追加

---
+++ laravel/app/OpenApi/Schemas/StoreUserSchema.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\OpenApi\Schemas;
+
+use GoldSpecDigital\ObjectOrientedOAS\Contracts\SchemaContract;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\AllOf;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\AnyOf;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Not;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\OneOf;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Schema;
+use Vyuldashev\LaravelOpenApi\Factories\SchemaFactory;
+
+class StoreUserSchema extends SchemaFactory
+{
+    /**
+     * @return AllOf|OneOf|AnyOf|Not|Schema
+     */
+    public function build(): SchemaContract
+    {
+        return Schema::object('StoreUser')
+            ->properties(
+                Schema::string('name')
+                    ->description('ユーザーのフルネーム')
+                    ->example('山田 太郎'),
+                Schema::string('email')
+                    ->format(Schema::TYPE_STRING)
+                    ->description('有効なメールアドレス')
+                    ->example('taro.yamada@example.com'),
+                Schema::string('password')
+                    ->format(Schema::FORMAT_PASSWORD)
+                    ->description('パスワード（8文字以上）')
+                    ->example('password123')
+            )
+            // 全てのフィールドを必須項目としてマーク
+            ->required('name', 'email', 'password');
+    }
+
+}
