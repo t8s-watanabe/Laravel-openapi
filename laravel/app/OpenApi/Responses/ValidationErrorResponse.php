439aba4c141b3e91a6fe8aa37909538f3fdbd4ec
watanabe-yuya <yuya.watanabe@t8s.co.jp>
2025-08-27 11:55:01 +0900

    validationエラースキーマ作成 ユーザー作成リクエストボディ作成

---
+++ laravel/app/OpenApi/Responses/ValidationErrorResponse.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\OpenApi\Responses;
+
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Response;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\MediaType;
+use Vyuldashev\LaravelOpenApi\Factories\ResponseFactory;
+use App\OpenApi\Schemas\ValidationErrorSchema;
+
+class ValidationErrorResponse extends ResponseFactory
+{
+    public function build(): Response
+    {
+        return Response::unprocessableEntity()
+            ->description('Validation failed')
+            ->content(
+                MediaType::json()->schema(ValidationErrorSchema::ref())
+            );
+    }
+}
